41a7e25a3008a05f075e5b52f468abc432638443
Geovane Batista Vital <geovanevital@Geovanes-Mac-mini.local>
2022-02-02 14:35:34 -0300

    apply react navigation structure

--- App.tsx
+++ App.tsx
@@ -8,7 +8,8 @@ import {
   Poppins_700Bold
 } from '@expo-google-fonts/poppins';
 import theme from './src/global/styles/theme'
-import { Dashboard } from './src/screens/Dashboard';
+import { NavigationContainer } from '@react-navigation/native'
+import { AppRoutes } from './src/routes/app.routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -23,7 +24,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <NavigationContainer>
+      <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
